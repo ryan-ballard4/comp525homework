8882095990a718eb19c4ac53759d2c9d40090264
Ryan Ballard <ryanballard4@yahoo.com>
2018-09-11 20:35:13 -0400

    implement circle area function

--- h1.py
+++ h1.py
@@ -4,3 +4,15 @@ h1.py
 Ryan Ballard
 9/12/2018
 """
+
+def circle_area(radius = float):
+    """
+    Calculates the area of a circle with given radius.
+    radius: non-negative float
+    Returns: float
+    """
+    import math
+    radius = float (input("What is the radius of the circle?"))
+    area = float
+    area = (math.pi *(radius * radius))
+    print("The area is", abs(area))
